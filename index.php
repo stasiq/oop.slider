4044db54f2cd32d68df5c34e4fcab8b730e3801e
s.konovalov <s.konovalov@intensa.ru>
2023-07-12 18:44:39 +0300

    oop черновой вариант

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="oop.css">
+</head>
+
+<body>
+<ul id="slides">
+    <li class="slide showing">Slide 1</li>
+    <li class="slide">Slide 2</li>
+    <li class="slide">Slide 3</li>
+    <li class="slide">Slide 4</li>
+    <li class="slide">Slide 5</li>
+</ul>
+
+<button class="controls" id="pause">Pause</button>
+<script src="oop.js"></script>
+</body>
+</html>
