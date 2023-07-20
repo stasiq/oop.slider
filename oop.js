6ff92662dc1064b83893c04936bd2b0c9fdd278c
s.konovalov <s.konovalov@intensa.ru>
2023-07-20 14:55:00 +0300

    Функции бинда кнопок вынесены за тело класса, в работе переключение предыдущего слайда

--- oop.js
+++ oop.js
@@ -4,11 +4,46 @@ document.addEventListener('DOMContentLoaded', function () {
         slides = document.querySelectorAll('#slides .slide');
         currentSlide = 0
         playing = true;
-        pauseButton = document.getElementById('pause');
 
         constructor() {
             this.slideInterval = setInterval(this.nextSlide.bind(this), 2000);
-            // this.pauseButton.onclick = this.start
+        }
+
+        nextSlide() {
+            this.slides[this.currentSlide].className = 'slide';
+            this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+            console.log('next' + this.currentSlide);
+            this.slides[this.currentSlide].className = 'slide showing';
+        }
+
+        prevSlide() {
+            console.log(this.currentSlide);
+            if (this.currentSlide < 2) {
+
+                this.currentSlide = 4;
+                console.log('меньше' + this.currentSlide);
+            }
+            this.slides[this.currentSlide].className = 'slide';
+            this.currentSlide = (this.currentSlide + 1) % this.slides.length - 2;
+            this.slides[this.currentSlide].className = 'slide showing';
+        }
+
+        start() {
+            if (this.playing) {
+                this.pauseSlideshow();
+            } else {
+                this.playSlideshow();
+            }
+        }
+
+    }
+
+    class SliperPanel extends Slider {
+        pauseButton = document.getElementById('pause');
+        nextButton = document.getElementById('next');
+        prevButton = document.getElementById('prev');
+        constructor() {
+            super();
         }
 
         playSlideshow() {
@@ -27,25 +62,18 @@ document.addEventListener('DOMContentLoaded', function () {
             this.pauseButton.innerHTML = 'Play';
             this.playing = false;
             clearInterval(this.slideInterval);
+            super.nextSlide();
         }
 
-        nextSlide() {
-            this.slides[this.currentSlide].className = 'slide';
-            this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-            this.slides[this.currentSlide].className = 'slide showing';
+        prevSlideButton() {
+            this.pauseButton.innerHTML = 'Play';
+            this.playing = false;
+            clearInterval(this.slideInterval);
+            super.prevSlide();
         }
-
-        start() {
-            if (this.playing) {
-                this.pauseSlideshow();
-            } else {
-                this.playSlideshow();
-            }
-        }
-
     }
 
-    const slider = new Slider();
+    const slider = new SliperPanel();
     slider.pauseButton.addEventListener("click", function () {
         if (slider.playing) {
             slider.pauseSlideshow();
@@ -53,6 +81,14 @@ document.addEventListener('DOMContentLoaded', function () {
             slider.playSlideshow();
 
         }
+    });
+
+    slider.nextButton.addEventListener("click", function () {
+        slider.nextSlideButton()
+    });
+
+    slider.prevButton.addEventListener("click", function () {
+        slider.prevSlideButton()
     });
     // Тут slider.playing undefined
     // slider.pauseButton.addEventListener("click", slider.start);
